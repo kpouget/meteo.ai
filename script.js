8c68bf32551850bf73427296e8c10375ba9664d7
Kevin Pouget <kpouget@redhat.com>
2025-11-04 21:06:18 +0100

    [gemini] fix: update plot_url for rain_total_day The user wants the plot_url to be consistent with the query. This commit updates the plot_url for rain_total_day to use a 24h range.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var METRICS = {
     "rain_total_day": {
         "query": "increase(rain{group=\"wundeground\", instance=\"home.972.ovh:35007\", job=\"raspi sensors\", mode=\"total\"}[24h])",
         "unit": "mm",
-        "plot_url": "https://prometheus.972.ovh/graph?g0.expr=increase(rain%7Bgroup%3D%22wundeground%22%2C%20instance%3D%22home.972.ovh%3A35007%22%2C%20job%3D%22raspi%20sensors%22%2C%20mode%3D%22total%22%7D%5B24h%5D)&g0.tab=0&g0.range_input=1d"
+        "plot_url": "https://prometheus.972.ovh/graph?g0.expr=increase(rain%7Bgroup%3D%22wundeground%22%2C%20instance%3D%22home.972.ovh%3A35007%22%2C%20job%3D%22raspi%20sensors%22%2C%20mode%3D%22total%22%7D%5B24h%5D)&g0.tab=0&g0.range_input=24h"
     },
     "rain_total_week": {
         "query": "increase(rain{group=\"wundeground\", instance=\"home.972.ovh:35007\", job=\"raspi sensors\", mode=\"total\"}[7d])",
