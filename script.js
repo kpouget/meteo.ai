6230fe0948e9943397533606edb0019a4acbd736
Kevin Pouget <kpouget@redhat.com>
2025-11-04 21:13:08 +0100

    [gemini] feat: update wind metrics to use 10m avg The user wants to see the average wind speed and gust over the last 10 minutes. This commit updates the queries and plot_urls for wind_speed and wind_gust to use avg_over_time over a 10m range.

--- script.js
+++ script.js
@@ -45,14 +45,14 @@ var METRICS = {
         "plot_url": "https://prometheus.972.ovh/graph?g0.expr=avg_over_time(temperature%7Bgroup%3D%22wundeground%22%2C%20instance%3D%22home.972.ovh%3A35007%22%2C%20job%3D%22raspi%20sensors%22%2C%20location%3D%22toiture%22%2C%20mode%3D%22dew_point%22%7D%5B10m%5D)&g0.tab=0&g0.range_input=10m"
     },
     "wind_speed": {
-        "query": "max_over_time(wind{group=\"wundeground\", instance=\"home.972.ovh:35007\", job=\"raspi sensors\", mode=\"speed\"}[10m])",
+        "query": "avg_over_time(wind{group=\"wundeground\", instance=\"home.972.ovh:35007\", job=\"raspi sensors\", mode=\"speed\"}[10m])",
         "unit": "km/h",
-        "plot_url": "https://prometheus.972.ovh/graph?g0.expr=max_over_time(wind%7Bgroup%3D%22wundeground%22%2C%20instance%3D%22home.972.ovh%3A35007%22%2C%20job%3D%22raspi%20sensors%22%2C%20mode%3D%22speed%22%7D%5B10m%5D)&g0.tab=0&g0.range_input=10m"
+        "plot_url": "https://prometheus.972.ovh/graph?g0.expr=avg_over_time(wind%7Bgroup%3D%22wundeground%22%2C%20instance%3D%22home.972.ovh%3A35007%22%2C%20job%3D%22raspi%20sensors%22%2C%20mode%3D%22speed%22%7D%5B10m%5D)&g0.tab=0&g0.range_input=10m"
     },
     "wind_gust": {
-        "query": "max_over_time(wind{group=\"wundeground\", instance=\"home.972.ovh:35007\", job=\"raspi sensors\", mode=\"gust\"}[10m])",
+        "query": "avg_over_time(wind{group=\"wundeground\", instance=\"home.972.ovh:35007\", job=\"raspi sensors\", mode=\"gust\"}[10m])",
         "unit": "km/h",
-        "plot_url": "https://prometheus.972.ovh/graph?g0.expr=max_over_time(wind%7Bgroup%3D%22wundeground%22%2C%20instance%3D%22home.972.ovh%3A35007%22%2C%20job%3D%22raspi%20sensors%22%2C%20mode%3D%22gust%22%7D%5B10m%5D)&g0.tab=0&g0.range_input=10m"
+        "plot_url": "https://prometheus.972.ovh/graph?g0.expr=avg_over_time(wind%7Bgroup%3D%22wundeground%22%2C%20instance%3D%22home.972.ovh%3A35007%22%2C%20job%3D%22raspi%20sensors%22%2C%20mode%3D%22gust%22%7D%5B10m%5D)&g0.tab=0&g0.range_input=10m"
     },
     "wind_dir": {
         "query": "avg_over_time(wind_dir{group=\"wundeground\", instance=\"home.972.ovh:35007\", job=\"raspi sensors\"}[10m])",
